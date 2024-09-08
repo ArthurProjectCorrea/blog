c60a817aaeb5b02670f46d1b5976752a51054db2
Arthur de Paula Correa <arthur.correa@DSS.LOCAL>
2024-09-08 00:43:18 -0300

    Develop: NavBar

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -1,14 +1,21 @@
 <template>
     <nav class="styleMain">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <div class="border border-red-500">
+            <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="75" height="" /> 
+        </div>
+        <div>
+            <div>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </div>
+        </div>
     </nav>
 </template>
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink } from "vue-router";
 </script>
 <style scoped>
 .styleMain {
-    @apply border border-red-500
+    @apply flex justify-between items-center border-gray-200  bg-gray-50 border-b-2 p-2
 }
 </style>
